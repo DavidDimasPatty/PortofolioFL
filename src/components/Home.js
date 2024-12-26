10e00ee542f194c3b45a50dad33b579f2ec89205
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2024-12-27 01:15:38 +0700

    button header

--- src/components/Home.js
+++ src/components/Home.js
@@ -90,7 +90,7 @@ const Home = () => {
 
                             <div className="row-md-5 d-flex justify-content-start align-items-center gap-5">
                                 <button className="btn btn-dark button-contact">Contact Us!</button>
-                                <button className="btn btn-dark button-plan">See Our Plan</button>
+                                <button className="btn btn-dark button-plan"><span>See Our Plan</span></button>
                             </div>
 
                         </div>
