601b1e0f1a389fa3b4c18ed1e5414ea82437d623
Nando WItin <108390976+nandowitin0602@users.noreply.github.com>
2025-01-06 20:41:24 +0700

    Revisi warna section Why TFC, Our Working Process, dan FaQ

--- src/components/WhyTfc.js
+++ src/components/WhyTfc.js
@@ -98,7 +98,7 @@ const WhyTfc = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 130">
                     <defs>
                         <linearGradient id="gradient-top" x1="0%" y1="0%" x2="100%" y2="0%">
-                            <stop offset="0%" stop-color="#023047" />
+                            <stop offset="0%" stop-color="#0d5c84" />
                             <stop offset="50%" stop-color="#8ecae6" />
                             <stop offset="100%" stop-color="#219ebc" />
                         </linearGradient>
@@ -137,7 +137,7 @@ const WhyTfc = () => {
                         <linearGradient id="gradient-bottom" x1="0%" y1="0%" x2="100%" y2="0%">
                             <stop offset="0%" stop-color="#219ebc" />
                             <stop offset="50%" stop-color="#8ecae6" />
-                            <stop offset="100%" stop-color="#023047" />
+                            <stop offset="100%" stop-color="#0d5c84" />
                         </linearGradient>
                     </defs>
                     <path d="M0,96L48,101.3C96,107,192,117,288,122.7C384,128,480,128,576,117.3C672,107,768,85,864,74.7C960,64,1056,64,1152,74.7C1248,85,1344,107,1392,117.3L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
