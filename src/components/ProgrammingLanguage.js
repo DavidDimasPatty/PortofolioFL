59baa6f818062487ba92c22c2ee6a1977b3dcd45
Nando WItin <108390976+nandowitin0602@users.noreply.github.com>
2025-01-06 13:26:46 +0700

    Revisi FaQ

---
+++ src/components/ProgrammingLanguage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../assets/programming-language.css";
+
+const ProgrammingLanguage = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default ProgrammingLanguage;
